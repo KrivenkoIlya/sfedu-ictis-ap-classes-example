3470c32baeec7c5c2fb3f83872c3f8d00f19e3aa
KrivenkoIlya <100480819+KrivenkoIlya@users.noreply.github.com>
2022-04-26 10:45:35 +0300

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -22,3 +22,45 @@ int main()
     construct c;
     cout << "a: " << c.a<< "\n" << "b: " << c.b;
 }
+
+
+
+
+class Person
+{
+public:
+    Person(string n)
+    {
+        name = n;
+    }
+    ~Person()
+    {
+        cout << name << "\n";
+    }
+private:
+    string name;
+};
+int main()
+{
+    Person name1("name1");
+    Person* name2 = new Person("name2");
+    delete name2;
+}
+
+
+
+int perimeterRectangle(int a, int b)
+{
+	int perimeter = 2 * (a + b);
+	return perimeter;
+}
+int main()
+{
+	int a = 5;
+	int b = 6;
+	cout << "Perimeter = " << perimeterRectangle(a, b);
+}
+
+
+
+
