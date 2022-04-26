f4fc3cd8ccfef28541a6f5b7b31a549c6c071caf
KrivenkoIlya <100480819+KrivenkoIlya@users.noreply.github.com>
2022-04-26 10:37:24 +0300

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Circle.h"
+#include <iostream>
+using namespace std;
+
+class construct
+{
+public:
+    int a, b;
+    construct()
+    {
+        a = 5;
+        b = 9;
+    }
+};
+
+int main()
+{
+    construct c;
+    cout << "a: " << c.a<< "\n" << "b: " << c.b;
+}
